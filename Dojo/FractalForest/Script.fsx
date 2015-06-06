271347e96cb3b5d97d247b8202eeb70c34c0054e
Adam Pridmore <adam_pridmore@hotmail.com>
2015-06-06 20:32:49 +0100

    - Extracted drawing tree to a .fs module.  - Saves tree to disk.

--- Dojo/FractalForest/Script.fsx
+++ Dojo/FractalForest/Script.fsx
@@ -1,8 +1,11 @@
 ﻿open System
 open System.Drawing
 open System.Windows.Forms
+open Tree
 
-//#load "Tree.fs"
+#load "Tree.fs"
+
+System.Environment.CurrentDirectory <- "c:\\temp"
 
 // Create a form to display the graphics
 let width, height = 500, 500         
@@ -15,51 +18,10 @@ let graphics = Graphics.FromImage(image)
 //more beautiful images, even if it's slower.
 //Thanks to https://twitter.com/AlexKozhemiakin for the tip!
 graphics.SmoothingMode <- System.Drawing.Drawing2D.SmoothingMode.HighQuality
-let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
 
 box.Image <- image
 box.Dock <- DockStyle.Fill
 form.Controls.Add(box) 
-
-// Compute the endpoint of a line
-// starting at x, y, going at a certain angle
-// for a certain length. 
-let endpoint x y angle length =
-    x + length * cos angle,
-    y + length * sin angle
-
-let flip x = (float)height - x
-
-// Utility function: draw a line of given width, 
-// starting from x, y
-// going at a certain angle, for a certain length.
-let drawLine (target : Graphics) (brush : Brush) 
-             (x : float) (y : float) 
-             (angle : float) (length : float) (width : float) =
-    let x_end, y_end = endpoint x y angle length
-    let origin = new PointF((single)x, (single)(y |> flip))
-    let destination = new PointF((single)x_end, (single)(y_end |> flip))
-    let pen = new Pen(brush, (single)width)
-    target.DrawLine(pen, origin, destination)
-
-let draw x y angle length width = 
-    drawLine graphics brush x y angle length width
-
-let pi = Math.PI
-
-// Now... your turn to draw
-// The trunk
-//draw 250. 50. (pi*(0.5)) 100. 4.
-//let x, y = endpoint 250. 50. (pi*(0.5)) 100.
-//// first and second branches
-//draw x y (pi*(0.5 + 0.3)) 50. 2.
-//draw x y (pi*(0.5 - 0.4)) 50. 2.
-
-type branchParams = {
-  dirOffset: float;
-  lenPercent: float;
-  widthPercent: float
-}
 
 let randomSeed = new System.Random()
 
@@ -69,9 +31,8 @@ let randomFloat min max =
 let randomInt min max = 
   min + randomSeed.Next(max - min)
 
-let maxDepth = randomInt 8 10
-let numberOfSplits = randomInt 2 4
-let treeParams = seq{ 
+let generateTreeParams numberOfSplits = 
+  seq{ 
     for _ in 1..numberOfSplits do 
       yield {
         dirOffset = randomFloat -1. 1.;
@@ -79,30 +40,13 @@ let treeParams = seq{
         widthPercent = randomFloat 0.3 0.6
       }
     } 
-    
-treeParams |> Seq.iter (printfn "%A")
 
-let drawTree maxDepth treeParams =
-  let rec branch currentDepth x y dir len width =
-    draw x y dir len width
-  
-    let (x',y') = endpoint x y dir (len*0.95)
+let maxDepth = randomInt 8 10
+let numberOfSplits = randomInt 2 4
 
-    if currentDepth >= maxDepth then () |> ignore
-    else 
-      treeParams
-      |> Seq.iter (fun p -> branch (currentDepth+1) x' y' (dir+p.dirOffset) (len * p.lenPercent) (width*p.widthPercent))
+let treeParams = generateTreeParams numberOfSplits
+//treeParams |> Seq.iter (printfn "%A")
+treeParams |> drawTree graphics height maxDepth
 
-  branch 1 250. 10. (0.5*pi) 150. 10.
- 
-drawTree maxDepth treeParams
-
-form.ShowDialog()
-
-
-(* To do a nice fractal tree, using recursion is
-probably a good idea. The following link might
-come in handy if you have never used recursion in F#:
-http://en.wikibooks.org/wiki/F_Sharp_Programming/Recursion
-*)
-
+//form.ShowDialog()
+image.Save(sprintf "Tree_%i.bmp" (randomInt 0 10000))
---
+++ Dojo/FractalForest/Tree.fs
@@ -0,0 +1,51 @@
+﻿module Tree
+open System
+open System.Drawing
+open System.Windows.Forms
+
+type branchParams = {
+  dirOffset: float;
+  lenPercent: float;
+  widthPercent: float
+}
+
+let drawTree graphics height maxDepth treeParams =
+  let flip x = (float)height - x
+
+  let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
+  
+  // Compute the endpoint of a line
+  // starting at x, y, going at a certain angle
+  // for a certain length. 
+  let endpoint x y angle length =
+      x + length * cos angle,
+      y + length * sin angle
+
+  // Utility function: draw a line of given width, 
+  // starting from x, y
+  // going at a certain angle, for a certain length.
+  let drawLine (target : Graphics) (brush : Brush) 
+                (x : float) (y : float) 
+                (angle : float) (length : float) (width : float) =
+      let x_end, y_end = endpoint x y angle length
+      let origin = new PointF((single)x, (single)(y |> flip))
+      let destination = new PointF((single)x_end, (single)(y_end |> flip))
+      let pen = new Pen(brush, (single)width)
+      target.DrawLine(pen, origin, destination)
+
+  let draw x y angle length width = 
+      drawLine graphics brush x y angle length width
+
+  let pi = Math.PI
+
+  let rec branch currentDepth x y dir len width =
+    draw x y dir len width
+  
+    let (x',y') = endpoint x y dir (len*0.95)
+
+    if currentDepth >= maxDepth then () |> ignore
+    else 
+      treeParams
+      |> Seq.iter (fun p -> branch (currentDepth+1) x' y' (dir+p.dirOffset) (len * p.lenPercent) (width*p.widthPercent))
+
+  branch 1 250. 10. (0.5*pi) 150. 10.
