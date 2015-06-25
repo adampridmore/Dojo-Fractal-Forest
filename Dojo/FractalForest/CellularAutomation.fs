f75114d6ed0791370bf36de2c964a691cdd5cacd
Adam Pridmore <adam_pridmore@hotmail.com>
2015-06-25 14:04:41 +0100

    - Moved CellularAutomation code into a compiled module.

---
+++ Dojo/FractalForest/CellularAutomation.fs
@@ -0,0 +1,38 @@
+﻿module CellularAutomation
+
+let isAliveRule (cells:array<char>) = 
+  match cells with
+  | [|' ';' ';' '|] -> ' '
+  | [|' ';' ';'X'|] -> 'X'
+  | [|' ';'X';' '|] -> 'X'
+  | [|' ';'X';'X'|] -> 'X'
+  | [|'X';' ';' '|] -> 'X'
+  | [|'X';' ';'X'|] -> ' '
+  | [|'X';'X';' '|] -> ' '
+  | [|'X';'X';'X'|] -> ' '
+  | _ -> 'E'
+
+let getCellFromArray (array:array<char>) index =
+  match index with
+  | index when index < 0 -> ' '
+  | index when index >= Array.length array -> ' '
+  | _ -> array.[index]
+    
+
+let isAlive (row:array<char>) index =
+  [|
+    getCellFromArray row (index-1);
+    getCellFromArray row (index);
+    getCellFromArray row (index+1)
+  |] 
+  |> isAliveRule
+
+let nextRow (row:array<char>) = 
+  row
+  |> Seq.mapi (fun i _ -> isAlive row i)
+  |> Seq.toArray
+
+let rowSequence initialRow = 
+  Seq.unfold (fun previousRow -> let currentRow = nextRow previousRow
+                                 Some(previousRow, currentRow)) initialRow
+
--- Dojo/FractalForest/CellularAutomation.fsx
+++ Dojo/FractalForest/CellularAutomation.fsx
@@ -1,43 +1,15 @@
 ﻿// https://en.wikipedia.org/wiki/Cellular_automaton
 
-let isAliveRule (cells:array<char>) = 
-  match cells with
-  | [|' ';' ';' '|] -> ' '
-  | [|' ';' ';'X'|] -> 'X'
-  | [|' ';'X';' '|] -> 'X'
-  | [|' ';'X';'X'|] -> 'X'
-  | [|'X';' ';' '|] -> 'X'
-  | [|'X';' ';'X'|] -> ' '
-  | [|'X';'X';' '|] -> ' '
-  | [|'X';'X';'X'|] -> ' '
-  | _ -> 'E'
+#load "CellularAutomation.fs"
 
-let getCellFromArray (array:array<char>) index =
-  match index with
-  | index when index < 0 -> ' '
-  | index when index >= Array.length array -> ' '
-  | _ -> array.[index]
+open CellularAutomation
 
-let isAlive (row:array<char>) index =
-  [|
-    getCellFromArray row (index-1);
-    getCellFromArray row (index);
-    getCellFromArray row (index+1)
-  |] 
-  |> isAliveRule
-    
 let arrayToString x = 
  x 
  |> Seq.map (fun item -> item.ToString())
  |> Seq.reduce (+)
  
 let toCharArray (x:string) = x.ToCharArray()
-
-
-let nextRow (row:array<char>) = 
-  row
-  |> Seq.mapi (fun i _ -> isAlive row i)
-  |> Seq.toArray
 
 let print x =
   printfn "%A" (x |> arrayToString)
@@ -46,9 +18,6 @@ let print x =
 let blankList length = 
   List.init length (fun _ -> ' ')
 
-let rowSequence initialRow = 
-  Seq.unfold (fun previousRow -> let currentRow = nextRow previousRow
-                                 Some(previousRow, currentRow)) initialRow
 
 let width = 200
 let row = (
@@ -59,3 +28,9 @@ let row = (
 (rowSequence row)
 |> Seq.take 100
 |> Seq.iter (fun row -> printfn "%A" (row |> arrayToString))
+
+(rowSequence ("              X              " |> toCharArray))
+|> Seq.take 15
+|> Seq.iter (fun row -> printfn "%A" (row |> arrayToString))
+
+
